052e30d7899847267e4e9524f10a55c644ac8d00
Aaron Holmes <aholmes@Aarons-MacBook-Pro.local>
2013-04-27 21:38:43 -0400

    Added xml fixture

--- ruby/rspec_play/spec/peepcode_client_spec.rb
+++ ruby/rspec_play/spec/peepcode_client_spec.rb
@@ -2,8 +2,25 @@ require 'rspec'
 require_relative '../peepcode_client.rb'
 
 describe Wordpress::Comments::Client do
-	it "stores a URL" do
-		client = Wordpress::Comments::Client.new 'http://google.com'
-		expect(client.url).to eq 'http://google.com' 
+
+	let (:client) { Wordpress::Comments::Client.new 'http://mashable.com/comments/feed/' }
+	
+	describe "#initialize on client" do
+		it "stores a URL" do
+			
+			expect(client.url).to eq 'http://mashable.com/comments/feed/' 
+		end
+	end
+
+	describe "#parse XML" do
+
+		let(:xml) { File.read(File.join('spec','fixtures','feed.xml')) }
+
+		it "extracts the link" do
+			comments = client.parse xml
+			comment = comments.first
+			link = "http://mashable.com/2012/07/18/ipad-early-photos/comment-page-1/#comment-18239503"
+			expect(comment[:link]).to eq link
+		end
 	end
 end
